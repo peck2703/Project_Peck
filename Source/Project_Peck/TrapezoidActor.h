776cc3574159d6c307f1449694ce6f0e73576b08
peck2703 <mrpeckjr@gmail.com>
2019-01-20 17:51:05 -0600

    save Trapezoid

---
+++ Source/Project_Peck/TrapezoidActor.h
@@ -0,0 +1,33 @@
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
+#include "TrapezoidActor.generated.h"
+
+UCLASS()
+class PROJECT_PECK_API ATrapezoidActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATrapezoidActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual void PostActorCreated() override;
+	virtual void PostLoad() override;
+	virtual void GenerateBoxMesh();
+	virtual void CreateSquare();
+	virtual void CreateBoxMesh(FVector BoxRadius, TArray <FVector> & Vertices, TArray <int32> & Triangles, TArray <FVector> & Normals, TArray <FVector2D> & UVs,
+		TArray <FProcMeshTangent> & Tangents, TArray <FColor> & Colors);
+
+private:
+	UPROPERTY(VisibleAnywhere)
+		UProceduralMeshComponent* mesh;
+	
+};
